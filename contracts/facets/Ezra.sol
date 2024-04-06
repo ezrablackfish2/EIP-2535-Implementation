97ccb30c852f346b192e89f96d21715feb97ab15
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-04-06 16:34:03 +0300

    diamond battle test 1

---
+++ contracts/facets/Ezra.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.0;
+
+contract Ezra {
+    function ezra1() external {}
+
+    function ezra2() external {}
+
+    function ezra3() external {}
+
+    function ezra4() external {}
+
+    function ezra5() external {}
+
+    
+}
