24bb38d6346ccc722a4dd2df9f043745a2aef081
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-04-12 15:35:24 +0300

    test done

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,5 +23,14 @@ module.exports = {
       enabled: true,
       runs: 200
     }
-  }
+  },
+    networks: {
+    
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
+      accounts: ['0xa8e9aa87ee430950e906de74b75dd03bb7da20c7d6118d5efad467a185436acf', '0xd67460fd15ea87e041daec427378517174b1af247a06310c14e07afd6322356d', '0x53d3fe202f914a69ec77307767f2131d8743d2a205efc3288dc598cd025b07e7'],
+      timeout: 600000
+    }  
+    
+  },
 }
